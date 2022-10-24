f9b135f4932d583067ba134564986e192a0541df
Sagnik Das <sagnik07das@gmail.com>
2022-10-25 00:02:37 +0530

    add feature to save userMaps and save it to a local file

--- app/src/main/java/com/example/mapme/MainActivity.kt
+++ app/src/main/java/com/example/mapme/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.mapme
 
 import android.app.Activity
+import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
@@ -17,11 +18,12 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.mapme.model.Place
 import com.example.mapme.model.UserMap
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.io.*
 
 const val EXTRA_USER_MAP = "EXTRA_USER_MAP"
 const val EXTRA_MAP_TITLE = "EXTRA_MAP_TITLE"
 private const val TAG = "MainActivity"
-
+private const val FILENAME = "UserMaps.data"
 class MainActivity : AppCompatActivity() {
     private lateinit var rvMaps: RecyclerView
     private lateinit var fabCreateMap: FloatingActionButton
@@ -34,25 +36,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        activityResultLauncher =
-            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result->
-                if (result.resultCode == Activity.RESULT_OK && result.data != null) {
-                    val data = result.data
-                    // Do operations
-                    val userMap : UserMap= data!!.getSerializableExtra(EXTRA_USER_MAP) as UserMap
-                    Log.i(TAG,"onActivityResult with new map title ${userMap.title}")
-                    userMaps.add(userMap)
-                    mapsAdapter.notifyItemInserted(userMaps.size-1)
-                }
-
-            }
 
         // View binding
         rvMaps = findViewById(R.id.rvMaps)
         fabCreateMap = findViewById(R.id.fabCreateMap)
 
-
+        val userMapsFromFile = deserializeUserMaps(this)
         userMaps = generateSampleData().toMutableList()
+        userMaps.addAll(userMapsFromFile)
+
+
         //Set layout manager on the recycler view
         rvMaps.layoutManager = LinearLayoutManager(this)
         //Set adapter on the recycler view
@@ -73,6 +66,20 @@ class MainActivity : AppCompatActivity() {
             showAlertDialog()
         }
 
+        activityResultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+            { result->
+                if (result.resultCode == Activity.RESULT_OK && result.data != null) {
+                    val data = result.data
+                    // Do operations
+                    val userMap : UserMap= data!!.getSerializableExtra(EXTRA_USER_MAP) as UserMap
+                    Log.i(TAG,"onActivityResult with new map title -> ${userMap.title}")
+                    userMaps.add(userMap)
+                    mapsAdapter.notifyItemInserted(userMaps.size-1)
+                    serializeUserMaps(this, userMaps)
+                }
+
+            }
     }
 
     private fun openSecondActivityForResult(title: String) {
@@ -81,91 +88,45 @@ class MainActivity : AppCompatActivity() {
         activityResultLauncher.launch(intent)
     }
 
-    private fun generateSampleData(): List<UserMap> {
-        return listOf(
-            UserMap(
-                "Memories from University",
-                listOf(
-                    Place("Branner Hall", "Best dorm at Stanford", 37.426, -122.163),
-                    Place(
-                        "Gates CS building",
-                        "Many long nights in this basement",
-                        37.430,
-                        -122.173
-                    ),
-                    Place("Pinkberry", "First date with my wife", 37.444, -122.170)
-                )
-            ),
-            UserMap(
-                "January vacation planning!",
-                listOf(
-                    Place("Tokyo", "Overnight layover", 35.67, 139.65),
-                    Place("Ranchi", "Family visit + wedding!", 23.34, 85.31),
-                    Place("Singapore", "Inspired by \"Crazy Rich Asians\"", 1.35, 103.82)
-                )
-            ),
-            UserMap(
-                "Singapore travel itinerary",
-                listOf(
-                    Place("Gardens by the Bay", "Amazing urban nature park", 1.282, 103.864),
-                    Place(
-                        "Jurong Bird Park",
-                        "Family-friendly park with many varieties of birds",
-                        1.319,
-                        103.706
-                    ),
-                    Place("Sentosa", "Island resort with panoramic views", 1.249, 103.830),
-                    Place(
-                        "Botanic Gardens",
-                        "One of the world's greatest tropical gardens",
-                        1.3138,
-                        103.8159
-                    )
-                )
-            ),
-            UserMap(
-                "My favorite places in the Midwest",
-                listOf(
-                    Place(
-                        "Chicago",
-                        "Urban center of the midwest, the \"Windy City\"",
-                        41.878,
-                        -87.630
-                    ),
-                    Place("Rochester, Michigan", "The best of Detroit suburbia", 42.681, -83.134),
-                    Place(
-                        "Mackinaw City",
-                        "The entrance into the Upper Peninsula",
-                        45.777,
-                        -84.727
-                    ),
-                    Place("Michigan State University", "Home to the Spartans", 42.701, -84.482),
-                    Place("University of Michigan", "Home to the Wolverines", 42.278, -83.738)
-                )
-            ),
-            UserMap(
-                "Restaurants to try",
-                listOf(
-                    Place("Champ's Diner", "Retro diner in Brooklyn", 40.709, -73.941),
-                    Place("Althea", "Chicago upscale dining with an amazing view", 41.895, -87.625),
-                    Place("Shizen", "Elegant sushi in San Francisco", 37.768, -122.422),
-                    Place(
-                        "Citizen Eatery",
-                        "Bright cafe in Austin with a pink rabbit",
-                        30.322,
-                        -97.739
-                    ),
-                    Place(
-                        "Kati Thai",
-                        "Authentic Portland Thai food, served with love",
-                        45.505,
-                        -122.635
-                    )
-                )
-            )
-        )
+//    private fun getDataFile(context: Context) : File{
+//        Log.i(TAG,"Getting file from directory ${context.filesDir}")
+//        return File(context.filesDir, FILENAME)
+//    }
+//
+//    private fun serializeUserMaps(context: Context, userMaps: List<UserMap>){
+//        Log.i(TAG, "serializeUserMaps")
+//        ObjectOutputStream(FileOutputStream(getDataFile(context))).use { it.writeObject(userMaps) }
+//    }
+//
+//    private fun deserializeUserMaps(context: Context) :List<UserMap>{
+//        Log.i(TAG,"deserializeUserMaps")
+//        val dataFile = getDataFile(context)
+//        if(dataFile.exists()){
+//            Log.i(TAG,"Data file does not exists")
+//            return emptyList()
+//        }
+//        ObjectInputStream(FileInputStream(dataFile)).use { return it.readObject() as List<UserMap> }
+//    }
+
+    private fun serializeUserMaps(context: Context, userMaps: List<UserMap>) {
+        Log.i(TAG, "serializeUserMaps")
+        ObjectOutputStream(FileOutputStream(getDataFile(context))).use { it.writeObject(userMaps) }
     }
 
+    private fun deserializeUserMaps(context: Context) : List<UserMap> {
+        Log.i(TAG, "deserializeUserMaps")
+        val dataFile = getDataFile(context)
+        if (!dataFile.exists()) {
+            Log.i(TAG, "Data file does not exist yet")
+            return emptyList()
+        }
+        ObjectInputStream(FileInputStream(dataFile)).use { return it.readObject() as List<UserMap> }
+    }
+
+    private fun getDataFile(context: Context) : File {
+        Log.i(TAG, "Getting file from directory ${context.filesDir}")
+        return File(context.filesDir, FILENAME)
+    }
     private fun showAlertDialog() {
         val mapFormView = LayoutInflater.from(this).inflate(R.layout.dialog_create_map, null)
         val dialog = AlertDialog.Builder(this)
@@ -190,4 +151,90 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+}
+
+private fun generateSampleData(): List<UserMap> {
+    return listOf(
+        UserMap(
+            "Memories from University",
+            listOf(
+                Place("Branner Hall", "Best dorm at Stanford", 37.426, -122.163),
+                Place(
+                    "Gates CS building",
+                    "Many long nights in this basement",
+                    37.430,
+                    -122.173
+                ),
+                Place("Pinkberry", "First date with my wife", 37.444, -122.170)
+            )
+        ),
+        UserMap(
+            "January vacation planning!",
+            listOf(
+                Place("Tokyo", "Overnight layover", 35.67, 139.65),
+                Place("Ranchi", "Family visit + wedding!", 23.34, 85.31),
+                Place("Singapore", "Inspired by \"Crazy Rich Asians\"", 1.35, 103.82)
+            )
+        ),
+        UserMap(
+            "Singapore travel itinerary",
+            listOf(
+                Place("Gardens by the Bay", "Amazing urban nature park", 1.282, 103.864),
+                Place(
+                    "Jurong Bird Park",
+                    "Family-friendly park with many varieties of birds",
+                    1.319,
+                    103.706
+                ),
+                Place("Sentosa", "Island resort with panoramic views", 1.249, 103.830),
+                Place(
+                    "Botanic Gardens",
+                    "One of the world's greatest tropical gardens",
+                    1.3138,
+                    103.8159
+                )
+            )
+        ),
+        UserMap(
+            "My favorite places in the Midwest",
+            listOf(
+                Place(
+                    "Chicago",
+                    "Urban center of the midwest, the \"Windy City\"",
+                    41.878,
+                    -87.630
+                ),
+                Place("Rochester, Michigan", "The best of Detroit suburbia", 42.681, -83.134),
+                Place(
+                    "Mackinaw City",
+                    "The entrance into the Upper Peninsula",
+                    45.777,
+                    -84.727
+                ),
+                Place("Michigan State University", "Home to the Spartans", 42.701, -84.482),
+                Place("University of Michigan", "Home to the Wolverines", 42.278, -83.738)
+            )
+        ),
+        UserMap(
+            "Restaurants to try",
+            listOf(
+                Place("Champ's Diner", "Retro diner in Brooklyn", 40.709, -73.941),
+                Place("Althea", "Chicago upscale dining with an amazing view", 41.895, -87.625),
+                Place("Shizen", "Elegant sushi in San Francisco", 37.768, -122.422),
+                Place(
+                    "Citizen Eatery",
+                    "Bright cafe in Austin with a pink rabbit",
+                    30.322,
+                    -97.739
+                ),
+                Place(
+                    "Kati Thai",
+                    "Authentic Portland Thai food, served with love",
+                    45.505,
+                    -122.635
+                )
+            )
+        )
+    )
 }
